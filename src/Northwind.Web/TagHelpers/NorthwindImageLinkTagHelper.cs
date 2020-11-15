1f502a107fde33a8bd8344d7644157f6f7381ab3
Igor Zakharov <ihar_zakharau@epam.com>
2020-11-15 23:47:28 +0300

    Add images tag helper

---
+++ src/Northwind.Web/TagHelpers/NorthwindImageLinkTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Northwind.Web.TagHelpers
+{
+    [HtmlTargetElement("a", Attributes = "northwind-id")]
+    public class NorthwindImageLinkTagHelper : TagHelper
+    {
+        public int NorthwindId { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context.TagName != "a")
+            {
+                throw new InvalidOperationException($"Tag helper '{nameof(NorthwindImageLinkTagHelper)}' can only be used for '<a>' tag.");
+            }
+
+            output.Attributes.SetAttribute("href", $"/images/{NorthwindId}");
+        }
+    }
+}
